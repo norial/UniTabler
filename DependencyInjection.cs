78a4b9b5f026af74b329be87400a31563c7e742d
molls <molls@DESKTOP-7LGDG75>
2025-01-22 00:28:56 +0200

    Добавьте файлы проекта.

---
+++ DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using UniTabler.BLL.CSVParserFactory;
+using UniTabler.Common.Interfaces.CSVParser;
+using UniTabler.Common.Interfaces.CSVParserFactory;
+using UniTabler.Common.Mappers;
+using UniTabler.BLL.CSVParsers;
+using UniTabler.BLL.RideInformation;
+using UniTabler.Common.Interfaces.RideInformation;
+using UniTabler.DAL.CSVParserRepository;
+
+namespace UniTabler.Common
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapperConfig));
+
+            services.AddHttpClient();
+
+            services.AddScoped<ICSVParserFactory, CSVParserFactory>();
+            services.AddScoped<ICSVParserRepository, CSVParserRepository>();
+            services.AddScoped<IRideInformationManager, RideInformationManager>();
+            services.AddScoped<IRideInformationRepository, RideInformationRepository>();
+
+
+            return services;
+        }
+    }
+}
