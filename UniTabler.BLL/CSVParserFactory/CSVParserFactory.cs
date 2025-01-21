29b616091617aae5db90b216eeee8c4b10e76065
Olejjas <mollstam4455@gmail.com>
2025-01-22 00:43:19 +0200

    Add files via upload

---
+++ UniTabler.BLL/CSVParserFactory/CSVParserFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using UniTabler.BLL.CSVParsers;
+using UniTabler.Common.Enums;
+using UniTabler.Common.Interfaces.CSVParser;
+using UniTabler.Common.Interfaces.CSVParserFactory;
+
+namespace UniTabler.BLL.CSVParserFactory
+{
+    public class CSVParserFactory : ICSVParserFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CSVParserFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ICSVParserManager CreateParser(SourceTypeEnum sourceType, string source)
+        {
+            var repository = _serviceProvider.GetRequiredService<ICSVParserRepository>();
+            var mapper = _serviceProvider.GetRequiredService<IMapper>();
+
+            switch (sourceType)
+            {
+                case SourceTypeEnum.File:
+                    return new FileCSVParserManager(source, repository, mapper);
+
+                case SourceTypeEnum.Web:
+                    return new WebCSVParserManager(source, repository, mapper);
+
+                default:
+                    throw new ArgumentException($"Unknown source type: {sourceType}");
+            }
+        }
+    }
+}
---
+++ UniTabler.Common/Mappers/TripRecordMap.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper.Configuration;
+using UniTabler.Common.Models;
+using UniTabler.Utils;
+
+namespace UniTabler.Common.Mappers
+{
+    public class TripRecordMap : ClassMap<TripRecordModel>
+    {
+        public TripRecordMap()
+        {
+            Map(m => m.PickUpDateTime)
+                .Name("tpep_pickup_datetime")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<EstToUtcDateTimeConverter>();
+            Map(m => m.DropOffDateTime)
+                .Name("tpep_dropoff_datetime")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<EstToUtcDateTimeConverter>();
+            Map(m => m.PassengerCount)
+                .Name("passenger_count")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<IntConverterUtil>();
+            Map(m => m.TripDistance)
+                .Name("trip_distance")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<SafeDoubleConverterUtil>();
+            Map(m => m.StoreAndForwardFlag)
+                .Name("store_and_fwd_flag")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<BoolConverterUtil>();
+            Map(m => m.PickUpLocationId)
+                .Name("PULocationID")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<IntConverterUtil>();
+            Map(m => m.DropOffLocationId)
+                .Name("DOLocationID")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<IntConverterUtil>();
+            Map(m => m.FareAmount)
+                .Name("fare_amount")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<SafeDecimalConverterUtil>();
+            Map(m => m.TipAmount)
+                .Name("tip_amount")
+                .TypeConverter<TrimmedStringConverterUtil>()
+                .TypeConverter<SafeDecimalConverterUtil>();
+        }
+    }
+}
